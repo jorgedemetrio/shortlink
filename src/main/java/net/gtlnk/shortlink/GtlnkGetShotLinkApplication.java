edfd61eb1c0e62a89051aeeda70502a03834bce1
Jorge Demetrio Sobieski Teixeira <jorge.demetrio@alldreams.com.br>
2023-03-23 23:51:28 -0300

    atualizando

---
+++ src/main/java/net/gtlnk/shortlink/GtlnkGetShotLinkApplication.java
@@ -0,0 +1,13 @@
+package net.gtlnk.shortlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtlnkGetShotLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtlnkGetShotLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/gtlnk/shortlink/GtlnkGetShotLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gtlnk.shortlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtlnkGetShotLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
